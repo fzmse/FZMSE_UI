c4d66527641a58d8e33c83959b8cbe31bce7623b
FertNokia <pawel.fert@nokia.com>
2015-08-06 16:00:39 +0200

    commit

--- Includes/Gui/appGui.h
+++ Includes/Gui/appGui.h
@@ -1,8 +1,6 @@
-#ifndef APPGUI_H
-#define APPGUI_H
 
 #include <QMainWindow>
-#include <QMdiArea>
+#include "TinyXML/tinyxml2.h"
 #include "Xml/XmlReader.h"
 #include "Xml/XmlElementReader.h"
 #include "Xml/XmlWrapper.h"
@@ -14,6 +12,9 @@ class QMenu;
 class QGroupBox;
 class QPushButton;
 class QTextEdit;
+class QTreeView;
+class QStandardItemModel;
+class QStandardItem;
 
 class appGUI : public QMainWindow
 {
@@ -23,9 +24,10 @@ public:
     appGUI();
 
 
+    void createLoadPathActions();
+    std::vector<QString> parseXmlByEndLine(std::string XML);
 private slots:
-    void openOldPDDB();
-    void openNewPDDB();
+    void loadPathTo(const QString&);
     void save();
     void generateRaport();
     void help();
@@ -55,14 +57,37 @@ private:
     QToolBar * raportToolBar;
     QAction * openOldPDDBAct;
     QAction * openNewPDDBAct;
+    QAction * openOldGMCAct;
+    QAction * openNewGMCAct;
     QAction * saveFileAct;
     QAction * generateRaportAct;
     QAction * displayHelpAct;
     QPushButton * clearListsBut;
     QPushButton * compareBut;
 
+    QTreeView * resultTreeView;
+    QStandardItemModel * resultStandardModel;
+    QStandardItemModel * subModel;
+
+    QList<QString *> listOfLabels(const int& n);
+
+    QList<QStandardItem *> listMOCTemplate(const QString& className);
+
+    QList<QStandardItem *> subListResultTypes(const QString& name,
+                                              const QString& count);
+
+    QList<QStandardItem *> listResultType(const QString& name,
+                                       const QString& added,
+                                       const QString& removed,
+                                       const QString& modified);
+
     XmlReader * reader;
     XmlElementReader * elementReader;
+
+    std::string oldPDDBPath;
+    std::string newPDDBPath;
+    std::string oldGMCPath;
+    std::string newGMCPath;
+
 };
 
-#endif // APPGUI_H
